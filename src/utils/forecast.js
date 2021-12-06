34b87c33a141108189cdbb12aa28ae793c5517d1
DB GUPTA <deshbandhugupta4@gmail.com>
2021-12-06 16:10:13 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request')
+const forecast = (longitude, latitude, callback) => {
+
+    const url = 'http://api.weatherstack.com/current?access_key=700830cb844fcd3f993bac90a66a3dde&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({ url, json: true }, (error, { body }) => {
+        if(error) {
+            callback('Unable to connect...', undefined)
+        }
+        else if(body.error) { 
+            callback('Unable to find Location', undefined)
+        }
+        else {
+            callback(undefined, 'It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.' )
+        }
+
+    }) 
+}
+
+
+
+module.exports = forecast
